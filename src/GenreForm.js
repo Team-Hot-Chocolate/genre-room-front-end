c8f89da49a2d763019672b40668a8cc5b22030ff
Simone Odegard <Simone.Odegard@gmail.com>
2021-04-06 11:03:10 -0700

    added genres/genre values

--- src/GenreForm.js
+++ src/GenreForm.js
@@ -21,19 +21,27 @@ class GenreForm extends React.Component {
               onChange={this.props.updateStateGenre}
             >
               <option value="0">Choose...</option>
-              <option value="Comedy">Comedy</option>
-              <option value="Action">Action</option>
-              <option value="Horror">Horror</option>
               <option value="Remove Genre">Remove Genre</option>
+              <option value="28">Action</option>
+              <option value="12">Adventure</option>
+              <option value="16">Animation</option>
+              <option value="35">Comedy</option>
+              <option value="80">Crime</option>
+              <option value="99">Documentary</option>
+              <option value="18">Drama</option>
+              <option value="10751">Family</option>
+              <option value="14">Fantasy</option>
+              <option value="36">History</option>
+              <option value="Horror">Horror</option>
+              <option value="10402">Music</option>
+              <option value="9648">Mystery</option>
+              <option value="10749">Romance</option>
+              <option value="878">Science Fiction</option>
+              <option value="10770">TV Movie</option>
+              <option value="53">Thriller</option>
+              <option value="10752">War</option>
+              <option value="37">Western</option>
             </Form.Control>
-          </Col>
-          <Col xs="auto" className="my-1">
-            <Form.Check
-              type="checkbox"
-              id="customControlAutosizing"
-              label="Remember my preference"
-              custom
-            />
           </Col>
           <Col xs="auto" className="my-1">
             <Button type="submit" onClick={this.props.updateGenre}>Submit</Button>
