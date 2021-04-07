742d843298d6458fa85fd563b27f2fd883b297ef
Simone Odegard <Simone.Odegard@gmail.com>
2021-04-07 15:02:15 -0700

    mvpppppp

--- src/Movie.js
+++ src/Movie.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Image from 'react-bootstrap/Image';
 
 class Movie extends React.Component {
   render() {
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Title>{this.props.title}</Card.Title>
-        <Card.Img variant="top" src={`https://www.themoviedb.org/t/p/original${this.props.src}`} />
-        <Card.Body>
-          <Card.Text>
-            {this.props.releaseDate}
-            {this.props.description}
-          </Card.Text>
-        </Card.Body>
-      </Card>
+      <center>
+        <Container>
+          <Row className="container">
+            <Col className="img" xs={6} md={4}>
+              <Image src={`https://www.themoviedb.org/t/p/original${this.props.src}`} thumbnail />
+            </Col>
+              <Card className="card" style={{ width: '18rem' }}>
+                <Card.Title>{this.props.title}</Card.Title>
+                {/* <Card.Img variant="top" src={`https://www.themoviedb.org/t/p/original${this.props.src}`} /> */}
+                <Card.Body>
+                  <Card.Text>
+                    {this.props.releaseDate}
+                    {this.props.description}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+          </Row>
+        </Container>
+      </center>
     )
   }
 }
