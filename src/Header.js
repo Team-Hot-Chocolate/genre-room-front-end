7f03e4e669bdbf9e91efa89460932834b7809c3c
Chris Gantt <gantt.art@gmail.com>
2021-04-08 09:09:19 -1000

    Updated header and removed profile.

--- src/Header.js
+++ src/Header.js
@@ -3,20 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import LogoutButton from './LogoutButton';
-import { withAuth0 } from '@auth0/auth0-react';
+import { withAuth0 } from "@auth0/auth0-react";
 
 
 class Header extends React.Component{
   render(){
     return(
       <Navbar className="bars" bg="black" variant="dark">
-        <Navbar.Brand href="/home">Genre Room</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/profile">Profile</Nav.Link>
-          {this.props.auth0.isAuthenticated &&
-          <LogoutButton />
-          }
+        <Navbar.Brand href="/" id='logo'>Genre Room</Navbar.Brand>
+        <Nav className="ml-auto">
+          
+          {this.props.auth0.isAuthenticated && 
+          <>
+            <Nav.Link>
+              <img src={this.props.auth0.user.picture} alt="user" id='header-user-image'/>
+            </Nav.Link>
+            <Nav.Link id='header-name'>{this.props.auth0.user.name}</Nav.Link>
+            <LogoutButton />
+          </>}
         </Nav>
       </Navbar>
     )
